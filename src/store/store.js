f5b93a5df489099623b4b71ba895b64d7d905b86
Muhammed Mukthar <muktharmanaf28@gmail.com>
2024-02-26 04:35:37 +0530

    update

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userIdSlice from "./userIdSlice";
 import questionsSlice from "./questionsSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, PERSIST } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const reducer = combineReducers({
+  userId: userIdSlice,
+  questions: questionsSlice,
+});
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+// Middleware configuration
+const middleware = (getDefaultMiddleware) => {
+  return getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, PERSIST],
+    },
+  });
+};
+
 const store = configureStore({
-  reducer: {
-    userId: userIdSlice,
-    questions: questionsSlice,
-  },
+  reducer: persistedReducer,
+  middleware,
 });
 
+export const persistor = persistStore(store);
 export default store;
